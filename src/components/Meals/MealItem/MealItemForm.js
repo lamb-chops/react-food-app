9810e60c3137c5dbc9f63f506952971020ad760a
David Lambert <delambert12@gmail.com>
2023-03-16 17:05:32 -0500

    wired up add button to reflect number of items in cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,22 +1,49 @@
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
+import { useRef, useState } from "react";
 
 const MealItemForm = (props) => {
-    //all input props are default built in props
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    //prevents page from reloading on submit
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    //value is alwasy string so + convers to int
+    const enteredAmountNumber = +enteredAmount;
+
+    //quick validation check
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmount < 1 ||
+      enteredAmount > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+  //all input props are default built in props
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
-          id: "amount_"+props.id,
+          id: "amount_" + props.id,
           type: "number",
           min: "1",
           max: "5",
-          step: '1',
-          defaultValue: '1'
+          step: "1",
+          defaultValue: "1",
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
     </form>
   );
 };
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,11 +1,15 @@
-import classes from './Input.module.css'
+import classes from "./Input.module.css";
+import React from "react";
 
-const Input = props => {
-    //spread operator adds all properties to input from props
-    return <div className={classes.input}>
-        <label htmlFor={props.input.id}>{props.label}</label>
-        <input {...props.input}/>
+//how to add refs to custom component
+const Input = React.forwardRef((props, ref) => {
+  //spread operator adds all properties to input from props
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input ref={ref} {...props.input} />
     </div>
-}
+  );
+});
 
-export default Input
+export default Input;
